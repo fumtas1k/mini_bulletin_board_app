d0e4c3ac6d8e2ea8e2e0475a105e8c7c34f16a35
fumtas1k <fumta01a7@gmail.com>
2022-01-07 23:00:16 +0900

    change:time_ago_in_wordsを自作の日本語表記に変更

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,21 @@ module PostsHelper
       post_path
     end
   end
+
+  def modified_time_ago_in_words(time)
+    secs = (Time.zone.now - time).round
+    days = secs / (60 * 60 * 24)
+    hours = secs / (60 * 60)
+    mins = secs / 60
+    if !days.zero?
+      time.strftime("%m月%d日")
+    elsif !hours.zero?
+      "#{hours}時間"
+    elsif !mins.zero?
+      "#{mins}分"
+    else
+      "#{secs}秒"
+    end
+  end
+
 end
