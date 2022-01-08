97921f975977befdf206ddb465c9d1c816cebad8
fumtas1k <fumta01a7@gmail.com>
2022-01-08 09:59:29 +0900

    improve:モーダルフォームを部分テンプレートに統一し、トップページからのみの呼び出しとした(不具合があるため)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def choose_new_or_edit
-    if ["new", "confirm", "create"].include?(action_name)
+    if ["new", "confirm", "create", "index"].include?(action_name)
       confirm_posts_path
     elsif action_name == "edit"
       post_path
